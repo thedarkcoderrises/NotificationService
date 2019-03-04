82144c1f9618a33489994a146bdf31c2f5d543d1
thedarkcoderrises <abhikrish88@gmail.com>
2019-03-04 21:18:59 +0530

    Jenkins pipeline

--- src/main/java/tdcr/notification/App.java
+++ src/main/java/tdcr/notification/App.java
@@ -2,14 +2,14 @@ package tdcr.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 
 /**
  * Hello world!
  *
  */
-//@SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = ContextRegionProviderAutoConfiguration.class)
+//@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
